9cffeb82877f00d65461b0dd9f2ea3fa97cb45c6
lucieneferri <78495231+lucieneferri@users.noreply.github.com>
2021-12-01 12:51:59 -0300

    add alguns dados na tabela (fixtures

--- countryapi/countries/models.py
+++ countryapi/countries/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Country(models.Model):
     name = models.CharField(max_length=100)
     capital = models.CharField(max_length=100)
-    carea = models. IntegerField(help_text='(in square kilometers)')
+    area = models. IntegerField(help_text='(in square kilometers)')
 
-    
+
